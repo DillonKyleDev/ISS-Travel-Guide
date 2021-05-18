e203ba0dca13891d16dde0c9552bb27e4785020a
DillonKyleDev <Dillonkyle95@yahoo.com>
2021-05-17 20:15:34 -0700

    More Components added

--- iss_travel_guide/src/App.js
+++ iss_travel_guide/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
+import HookCounterArray from './components/HookCounterArray';
 import HookCounterTwo from './components/HookCounterTwo';
+
 
 function App() {
   return (
     <div className="App">
-      <HookCounterTwo />
+      <HookCounterArray />
     </div>
   );
 }
