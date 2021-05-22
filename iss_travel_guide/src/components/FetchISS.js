643d7cad07120cfe8f9ce75a06f3ed2f7d6a3324
DillonKyleDev <Dillonkyle95@yahoo.com>
2021-05-22 11:27:15 -0700

    Added API components, DrawMap component, and DisplayResults components

--- iss_travel_guide/src/components/FetchISS.js
+++ iss_travel_guide/src/components/FetchISS.js
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
-import DrawMap from './DrawMap'
+import DrawMap from './DrawMap/DrawMap'
 import TomTomSearch from './TomTomSearch'
+import TitleText from './TitleText/TitleText'
 
 function FetchISS() {
-  const [lat, setLat] = useState(50);
+  const [lat, setLat] = useState(0);
   const [lon, setLon] = useState(0);
   useEffect(
     () => fetchISS()
@@ -12,15 +13,23 @@ function FetchISS() {
   async function fetchISS() {
     const iss_API = 'https://api.wheretheiss.at/v1/satellites/25544';
     const data = await fetch(iss_API);
-    const coords = await data.json();
-    setLat(coords.latitude);
-    setLon(coords.longitude);
+    const coords = await data.json()
+    .then(() => {
+      if(coords) {
+        setLat(coords.latitude);
+        setLon(coords.longitude);
+      }
+    }).catch((err) => {
+      console.error(err);
+    })
+    
   }
   
   //setTimeout(() => {fetchISS()}, 3000);
  
   return(
     <div>
+      <TitleText />
       <DrawMap lat={lat} lon={lon} />
       <TomTomSearch lat={lat} lon={lon}/>
     </div>
--- iss_travel_guide/src/components/TomTomSearch.js
+++ iss_travel_guide/src/components/TomTomSearch.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
-import DisplayResults from './DisplayResults'
-
+import DisplayResults from './DisplayResults/DisplayResults'
+import DisplaySearchMenu from './DisplaySearchMenu/DisplaySearchMenu'
 
 function TomTomSearch(props) {
   const {lat, lon} = props;
   const [ radius, setRadius ] = useState(10000);
   const [ keyword, setKeyword ] = useState('food');
-  const [ query, setQuery ] = useState('null');
+  const [ query, setQuery ] = useState({results: 'null'});
   let radios = document.getElementsByName('units');
 
   useEffect(
@@ -18,19 +18,19 @@ function TomTomSearch(props) {
     if(radios[0].checked) {
       tempRadius = radius * 1609.34;     
     }
-    //Fetch tomtom coordinate search endpoint
+    //Fetch TomTom coordinate search endpoint
     const tomtom_API = `https://api.tomtom.com/search/2/search/${keyword}.json?typeahead=false&limit=10&ofs=0&lat=${lat}&lon=${lon}&radius=${tempRadius}&language=en-US&minFuzzyLevel=1&maxFuzzyLevel=2&key=ZO8TcaaYrxWNrZPmiTpIyKepTgr5yPqU`;
     const coordQuery = await fetch(tomtom_API);
-    let queryTemp = await coordQuery.json();
-    setQuery(queryTemp);
-    if(query.results[0] === undefined) {
-      console.log('ISS didn"t find anything around there');
-      console.log(query.results);
-    } else {
-      console.log('Found some stuff!');
-      console.log(query.results);
-      }
+    await coordQuery.json()
+    .then((queryTemp) => {
+      if(queryTemp.results.length !== 0) {
+        setQuery(queryTemp);
+      } else {
+        setQuery({results: 'null'});
+        }
+      })
   }
+
   function changeRadius(e) {
     setRadius(e.target.value);
   }
@@ -40,19 +40,12 @@ function TomTomSearch(props) {
   
   return (
     <div>
-      <div id='search box'>
-        <label>Search Radius </label>
-        <input type='text' value={radius} onChange={e => changeRadius(e)}></input>
-        <input type='radio' name='units' value='Miles' defaultChecked></input>
-        <label>Miles</label>
-        <input type='radio' name='units' value='Meters'></input>
-        <label>Meters</label>
-        <br />
-        <label> Search Keyword </label>
-        <input type='text' value={keyword} onChange={e => changeKeyword(e)}></input>
-        <br />
-        <button onClick={getCoords}>Submit</button>
-      </div>
+      <DisplaySearchMenu 
+      changeRadius={e => changeRadius(e)}
+      changeKeyword={e => changeKeyword(e)}
+      getCoords={getCoords}
+      radius={radius}
+      keyword={keyword}/>
       <DisplayResults query={query}/>
     </div>
   )
